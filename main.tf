28a81ed25f3ecb078bff3bf6c62265afa6ec4eb7
Logesh Shanmugavel <logesh81098@gmail.com>
2025-07-03 14:48:38 +0530

    Updated IAM Policy for Node Group worked node to create Load Balancer

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source = "./module/s3"
+  source                              = "./module/s3"
   rekognition-faceprints-function-arn = module.lambda-function.rekognition-faceprints-function-arn
 }
 
@@ -10,8 +10,8 @@ module "iam" {
 module "lambda-function" {
   source                        = "./module/lambda-function"
   rekognition-collectionid-role = module.iam.rekognition-collectionid-role
-  rekognition-faceprints-role = module.iam.rekognition-faceprints-role
-  source-bucket-arn = module.s3.source-bucket-arn
+  rekognition-faceprints-role   = module.iam.rekognition-faceprints-role
+  source-bucket-arn             = module.s3.source-bucket-arn
 }
 
 module "dynamodb" {
@@ -32,28 +32,28 @@ module "key-pair" {
 }
 
 module "ec2-server" {
-  source = "./module/ec2-server"
-  keypair = module.key-pair.face-rekognition-server-keypair
-  subnet-id = module.vpc.public-subnet-1
-  security-group = module.security-group.rekognition-server-sg
+  source                                = "./module/ec2-server"
+  keypair                               = module.key-pair.face-rekognition-server-keypair
+  subnet-id                             = module.vpc.public-subnet-1
+  security-group                        = module.security-group.rekognition-server-sg
   Face-Recognition-EC2-Instance-Profile = module.iam.rekognition-flask-server-instance-profile
 }
 
 module "eks" {
-  source = "./module/eks"
-  cluster-role = module.iam.rekognition-cluster-role
-  subnet-1 = module.vpc.public-subnet-1
-  subnet-2 = module.vpc.public-subnet-2
-  eks-security-group = module.security-group.rekognition-eks-cluster-sg
+  source                     = "./module/eks"
+  cluster-role               = module.iam.rekognition-cluster-role
+  subnet-1                   = module.vpc.public-subnet-1
+  subnet-2                   = module.vpc.public-subnet-2
+  eks-security-group         = module.security-group.rekognition-eks-cluster-sg
   application-security-group = module.security-group.rekognition-server-sg
-  node-group-role = module.iam.rekognition-nodegroup-role
-  launch-template-id = module.launch-template.launch-template-id
-  
+  node-group-role            = module.iam.rekognition-nodegroup-role
+  launch-template-id         = module.launch-template.launch-template-id
+
 }
 
 module "launch-template" {
-  source = "./module/launch-template"
-  nodegroup-sg = module.security-group.rekognition-nodegroup-sg
+  source         = "./module/launch-template"
+  nodegroup-sg   = module.security-group.rekognition-nodegroup-sg
   application-sg = module.security-group.rekognition-server-sg
-  key-name = module.key-pair.face-rekognition-server-keypair
+  key-name       = module.key-pair.face-rekognition-server-keypair
 }
--- module/iam/main.tf
+++ module/iam/main.tf
@@ -399,6 +399,43 @@ EOF
 }
 
 
+
+
+##########################################################################################################################################
+#                                                   IAM Policy
+##########################################################################################################################################
+
+#IAM Policy for Node Group to create Load Balance and create Security Group
+
+resource "aws_iam_policy" "face-rekognition-k8s-policy" {
+  name = "Rekognition-k8s-policy"
+  description = "IAM Policy for Node Group to create Load Balance and create Security Group"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+          "ec2:Describe*",
+          "elasticloadbalancing:*",
+          "ec2:CreateSecurityGroup",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:DeleteSecurityGroup"
+      ],
+      "Resource": "*"
+    }
+  ]
+}  
+EOF
+  tags = {
+    Name = "Rekognition-k8s-policy"
+    Project = "Recognizing-faces-using-AWS-Rekognition-service"
+  }
+}
+
+
 ##########################################################################################################################################
 #                                                        Role Policy Attachement
 ##########################################################################################################################################
@@ -423,4 +460,9 @@ resource "aws_iam_role_policy_attachment" "eks-worker-node-policy" {
 resource "aws_iam_role_policy_attachment" "eks-application-node-group-policy" {
   role = aws_iam_role.eks-node-group-role.id
   policy_arn = aws_iam_policy.rekognition-flask-application-policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "eks-k8s-policy" {
+  role = aws_iam_role.eks-node-group-role.id
+  policy_arn = aws_iam_policy.face-rekognition-k8s-policy.arn
 }
